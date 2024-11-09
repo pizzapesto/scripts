55df84bb0f20581db024ab9c113911b52ad66fd1
rogermgual <roger.martingual@gmail.com>
2024-11-09 16:44:44 +0100

    RMG: Create repo and first version of the functions. TBD: API and test

---
+++ auto-ddns/App/main.py
@@ -0,0 +1,67 @@
+import requests
+import os
+
+# Import the variables from the vars.py file
+from vars import API_TOKEN, DNS_NAME, CF_DNS_URL
+
+def get_public_ip() -> str:
+    """Obtains the public IP of the machine.
+
+    Returns:
+        str: The public IP address.
+    """
+    response = requests.get('https://api.ipify.org?format=json')
+    return response.json().get('ip')
+
+def update_dns_record(ip:str) -> dict:
+    """Update the DNS record with the new IP.
+
+    Args:
+        ip (str): The new IP address.
+
+    Returns:
+        dict: The response from the Cloudflare API.
+    """
+    headers = {
+        'Authorization': f'Bearer {API_TOKEN}',
+        'Content-Type': 'application/json',
+    }
+    data = {
+        'type': 'A',
+        'name': DNS_NAME,
+        'content': ip,
+        'ttl': 1,
+        'proxied': False,
+    }
+    response = requests.put(CF_DNS_URL, headers=headers, json=data)
+    return response.json()
+
+def main():
+    # Obtains the current public IP
+    current_ip = get_public_ip()
+    ip_file = '/tmp/current_ip.txt'
+    
+    # Check if the file exists and read the last IP
+    if os.path.isfile(ip_file):
+        with open(ip_file, 'r') as f:
+            last_ip = f.read().strip()
+    else:
+        last_ip = None
+
+    # If the IP has changed, update the DNS record
+    if current_ip != last_ip:
+        # Update the DNS record
+        result = update_dns_record(current_ip)
+        # Check if the update was successful
+        if result['success']:
+            print(f"DNS updated: {current_ip}")
+            # Store the current IP in a file
+            with open(ip_file, 'w') as f:
+                f.write(current_ip)
+        else:
+            print("Error updtaing DNS record", result)
+    else:
+        print("No changes in the IP.")
+
+if __name__ == "__main__":
+    main()
---
+++ auto-ddns/App/vars.py
@@ -0,0 +1,8 @@
+# Configura tus datos de autenticación
+ZONE_ID = 'zone_id'
+DNS_RECORD_ID = 'dns_record_id'
+API_TOKEN = 'api_token'
+DNS_NAME = 'domain.com'
+
+# Endpoint de Cloudflare
+CF_DNS_URL = f'https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records/{DNS_RECORD_ID}'
